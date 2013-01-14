29f723b9bd4f8ff6ce208ac9963768db4eabb5e8
Esben Haabendal <esben.haabendal@prevas.dk>
2013-01-14 09:45:56 +0100

    overlay-files: Whitespace cleanup
    Signed-off-by: Esben Haabendal <esben.haabendal@prevas.dk>
    
    (cherry picked from commit 5d1ff13ad5c985fda5430d753c41e7a934d8877c)

--- recipes/overlay-files/files/overlay.sh
+++ recipes/overlay-files/files/overlay.sh
@@ -5,9 +5,9 @@ OVERLAY_DIR="${USE_overlay_files_dir}"
 OVERLAY_BASE="${USE_overlay_files_base}"
 
 for file in $OVERLAY_FILES ; do
-    if test -e $OVERLAY_DIR$file ; then
-        ln -sf $OVERLAY_DIR$file $OVERLAY_BASE$file
-    elif test -e `basename $file`.default ; then
-        ln -sf `basename $file`.default $OVERLAY_BASE$file
-    fi
+	if test -e $OVERLAY_DIR$file ; then
+		ln -sf $OVERLAY_DIR$file $OVERLAY_BASE$file
+	elif test -e `basename $file`.default ; then
+		ln -sf `basename $file`.default $OVERLAY_BASE$file
+	fi
 done
