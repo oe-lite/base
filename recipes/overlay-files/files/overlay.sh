2a54d7526e9b9b4d98712d7ca3b8001de78f8a9c
Esben Haabendal <esben@haabendal.dk>
2016-02-04 13:58:11 +0100

    overlay-files: Add USE_overlay_files_squash flag
    Set USE_overlay_files_squash flag to squash the paths in the overlay
    dir, and thus keep a flat file structure there.  Care must be taken to
    avoid duplicates!
    
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- recipes/overlay-files/files/overlay.sh
+++ recipes/overlay-files/files/overlay.sh
@@ -3,10 +3,15 @@
 OVERLAY_FILES=`cat ${OVERLAY_FILES_ETC} 2>/dev/null`
 OVERLAY_DIR="${USE_overlay_files_dir}"
 OVERLAY_BASE="${USE_overlay_files_base}"
+OVERLAY_SQUASH="${USE_overlay_files_squash}"
 
 for file in $OVERLAY_FILES ; do
     link_file="$OVERLAY_BASE$file"
-    overlay_file="$OVERLAY_DIR$file"
+    if test -n "$OVERLAY_SQUASH" -a "$OVERLAY_SQUASH" != "0" ; then
+	overlay_file="$OVERLAY_DIR/"`basename "$file"`
+    else
+	overlay_file="$OVERLAY_DIR$file"
+    fi
     if test ! -e "$overlay_file" ; then
 	overlay_file="$OVERLAY_BASE$file.default"
     fi
