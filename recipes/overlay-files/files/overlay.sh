8ddb14715ae590d8f6567728815877438fca79a4
Esben Haabendal <esben@haabendal.dk>
2016-02-04 09:22:06 +0100

    overlay-files: Whitespace change
    Reduce indent to ease integration of additional features without going
    above 79 columns.

--- recipes/overlay-files/files/overlay.sh
+++ recipes/overlay-files/files/overlay.sh
@@ -5,9 +5,9 @@ OVERLAY_DIR="${USE_overlay_files_dir}"
 OVERLAY_BASE="${USE_overlay_files_base}"
 
 for file in $OVERLAY_FILES ; do
-	if test -e $OVERLAY_DIR$file ; then
-		ln -sf $OVERLAY_DIR$file $OVERLAY_BASE$file
-	elif test -e ${OVERLAY_BASE}${file}.default ; then
-		ln -sf ${OVERLAY_BASE}${file}.default $OVERLAY_BASE$file
-	fi
+    if test -e $OVERLAY_DIR$file ; then
+	ln -sf $OVERLAY_DIR$file $OVERLAY_BASE$file
+    elif test -e ${OVERLAY_BASE}${file}.default ; then
+	ln -sf ${OVERLAY_BASE}${file}.default $OVERLAY_BASE$file
+    fi
 done
